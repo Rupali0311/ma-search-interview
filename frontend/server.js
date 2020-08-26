3bb04474de51009ba5ac433f6ab1da93789e31ed
Brandon Renfrow <brenfrow@moz.com>
2020-08-25 20:53:13 -0700

    add mongo database connection

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
+const { connect, model, Schema } = require('mongoose');
 
-const port = 3000;
+const PORT = 3000;
+
+const Contact = model('Contact', new Schema({
+  firstName: String,
+  lastName: String,
+  phoneNumber: String,
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+}));
+
+connect('mongodb://mongo:27017', { useNewUrlParser: true });
 
 app.get('/api', (req, res) => {
   res.json({});
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(PORT, () => console.log('API is running'));
